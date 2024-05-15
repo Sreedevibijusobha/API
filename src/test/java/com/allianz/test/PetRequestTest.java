7eefc967c080b46ca01227548b05e9a019fc1a84
Sreedevi <sreedevibiju5@gmail.com>
2024-05-15 10:45:12 +0530

    message

---
+++ src/test/java/com/allianz/test/PetRequestTest.java
@@ -0,0 +1,21 @@
+package com.allianz.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+
+public class PetRequestTest {
+	public static String baseUrl="https://petstore.swagger.io/v2";
+	@Test
+	
+	public void findValidPetTest()
+	{
+		int petId=10;
+		String resource="/pet/"+petId;
+		String jsonString=RestAssured.given().when().get(baseUrl+resource).then().statusCode(200).extract().asString();
+		
+		Assert.assertTrue(jsonString.contains(": 10"));
+	}
+
+}
